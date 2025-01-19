0eb92fc0b3cb021b0becd68549aadbaea00ee288
itu-itis24-kartalm24 <kartalm24@itu.edu.tr>
2025-01-19 13:53:15 +0300

    Updated game.js to be more clear with new functions

--- game.js
+++ game.js
@@ -42,13 +42,76 @@ function showLetters() {
     });
 }
 
+function handleGameStart(guess) {
+    if (!gameStarted && guess !== '') {
+        gameStarted = true;
+        resetButton.style.display = 'inline-block';
+    }
+}
+
+function handleFullWordGuess(guess) {
+    if (guess === currentWord) {
+        showLetters();
+        score = 100;
+        updateScore();
+        showWinMessage();
+        return true;
+    } else {
+        handleGameOver('Wrong word!');
+        return true;
+    }
+}
+
+function handleSingleLetterGuess(guess) {
+    if (currentWord.includes(guess)) {
+        let i = currentWord.indexOf(guess);
+        if (!revealedLetters.has(i)) {
+            revealLetterAt(i);
+            if (checkWin()) {
+                showWinMessage();
+            }
+        }
+    } else {
+        decrementLives();
+    }
+}
+
+function revealLetterAt(index) {
+    revealedLetters.add(index);
+    score += 20;
+    updateScore();
+    const letterImage = document.querySelector(`#box${index} .letter-image`);
+    letterImage.style.opacity = '1';
+}
+
+function decrementLives() {
+    lives--;
+    updateLives();
+    if (lives === 0) {
+        handleGameOver('Game Over!');
+    }
+}
+
+function showWinMessage() {
+    setTimeout(() => {
+        alert('Congratulations! You won! Your score: ' + score);
+        resetGame();
+    }, 100);
+}
+
+function handleGameOver(message) {
+    lives = 0;
+    updateLives();
+    alert(message + ' Your score: ' + score);
+    resetGame();
+}
+
 function resetGame() {
     hideLetters();
     score = 0;
     revealedLetters.clear();
     lives = 3;
     gameStarted = false;
-    hideLetters();
     updateScore();
     updateLives();
     predictionInput.value = '';
@@ -59,52 +122,15 @@ function handleGuess() {
     const guess = predictionInput.value.toUpperCase();
     predictionInput.value = '';
 
-    if (!gameStarted && guess !== '') {
-        gameStarted = true;
-        resetButton.style.display = 'inline-block';
-    }
+    handleGameStart(guess);
 
     if (guess.length === currentWord.length) {
-        if (guess === currentWord) {
-            showLetters();
-            setTimeout(() => {
-                alert('Congratulations! You won! Your score: ' + score);
-                resetGame();
-            }, 1000);
-        } else {
-            lives = 0;
-            updateLives();
-            alert('Wrong word! Game Over! Your score: ' + score);
-            resetGame();
-        }
+        handleFullWordGuess(guess);
         return;
     }
 
     if (guess.length === 1) {
-        if (currentWord.includes(guess)) {
-            let i = currentWord.indexOf(guess);
-            if (!revealedLetters.has(i)) {
-                revealedLetters.add(i);
-                score += 20;
-                updateScore();
-                const letterImage = document.querySelector(`#box${i} .letter-image`);
-                letterImage.style.opacity = '1';
-
-                if (checkWin()) {
-                    setTimeout(() => {
-                        alert('Congratulations! You won! Your score: ' + score);
-                        resetGame();
-                    }, 100);
-                }
-            }
-        } else {
-            lives--;
-            updateLives();
-            if (lives === 0) {
-                alert('Game Over! Your score: ' + score);
-                resetGame();
-            }
-        }
+        handleSingleLetterGuess(guess);
     }
 }
 
